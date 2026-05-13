7b8fc0d40cf11e793371dc3b70fc793afc01ffd6
Philipp Trentmann <philipp.trentmann@ledger.fr>
2026-05-13 22:23:14 +0200

    feat(LIVE-29447): add mobile swap status drawer

--- apps/ledger-live-mobile/src/reducers/index.ts
+++ apps/ledger-live-mobile/src/reducers/index.ts
@@ -22,6 +22,7 @@ import modularDrawer from "./modularDrawer";
 import receiveOptionsDrawer from "./receiveOptionsDrawer";
 import rebornBuyDeviceDrawer from "./rebornBuyDeviceDrawer";
 import transferDrawer from "./transferDrawer";
+import swapTransactionStatusDrawer from "./swapTransactionStatusDrawer";
 import notifications from "./notifications";
 import protect from "./protect";
 import ratings from "./ratings";
@@ -65,6 +66,7 @@ const appReducer = combineReducers({
   receiveOptionsDrawer,
   rebornBuyDeviceDrawer,
   transferDrawer,
+  swapTransactionStatusDrawer,
   notifications,
   postOnboarding,
   postOnboardingHubDrawer,
