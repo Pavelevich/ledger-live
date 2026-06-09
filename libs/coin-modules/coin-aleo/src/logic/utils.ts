724de321c14b593e58854a2a531381b7c073acc9
mateuszpalosz-ext <mateusz.palosz-ext@ledger.fr>
2026-06-09 12:48:01 +0200

    feat: moved public sync logic with extended base fixes

--- libs/coin-modules/coin-aleo/src/logic/utils.ts
+++ libs/coin-modules/coin-aleo/src/logic/utils.ts
@@ -161,6 +161,7 @@ export const toBridgeOperation = (
   ledgerAccountId: string,
   rawTx: AleoPublicTransaction,
   address: string,
+  isTokenTx?: boolean,
 ): AleoOperation => {
   const value = new BigNumber(rawTx.amount);
   const { type, fee, blockHash, transactionType, date, hasFailed } = parseTransactionFields(
@@ -188,6 +189,11 @@ export const toBridgeOperation = (
     extra: {
       functionId: rawTx.function_id,
       transactionType,
+      ...(isTokenTx && {
+        tokenInfo: {
+          programId: rawTx.program_id,
+        },
+      }),
     },
   };
 };
